0d05a7fbd9a4006066acff375eeaf9e355623231
Denis Lebedev <3d.lebedzeu@gmail.com>
2024-02-16 21:22:36 +0300

    add computing resource page

--- main.go
+++ main.go
@@ -8,9 +8,21 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/gorilla/mux"
 )
+
+type ThreadInfo struct {
+	LastPing time.Time
+	Status   string
+	Id       int
+}
+
+func (ti *ThreadInfo) UpdateStatus(newStatus string) {
+	ti.Status = newStatus
+	ti.LastPing = time.Now()
+}
 
 type ExpressionKey string
 
@@ -19,13 +31,16 @@ const (
 )
 
 var (
+	numberOfThreads = 10
 	db              = expressions.NewDB()
 	expressionsChan = make(chan *expressions.Expression, 1000)
+	threadInfos     = make([]*ThreadInfo, numberOfThreads)
 
-	inputExpressionTemplate = template.Must(template.ParseFiles("static/templates/inputExpressionTemplate.html"))
-	inputListTemplate       = template.Must(template.ParseFiles("static/templates/inputListTemplate.html"))
-	listExpressionsTemplate = template.Must(template.ParseFiles("static/templates/listExpressionsTemplate.html"))
-	configurationTemplate   = template.Must(template.ParseFiles("static/templates/configurationTemplate.html"))
+	inputExpressionTemplate    = template.Must(template.ParseFiles("static/templates/inputExpressionTemplate.html"))
+	inputListTemplate          = template.Must(template.ParseFiles("static/templates/inputListTemplate.html"))
+	listExpressionsTemplate    = template.Must(template.ParseFiles("static/templates/listExpressionsTemplate.html"))
+	configurationTemplate      = template.Must(template.ParseFiles("static/templates/configurationTemplate.html"))
+	computingResourcesTemplate = template.Must(template.ParseFiles("static/templates/computingResourcesTemplate.html"))
 )
 
 func InputExpressionHandler(w http.ResponseWriter, r *http.Request) {
@@ -68,6 +83,10 @@ func ChangeConfigurationHandler(w http.ResponseWriter, r *http.Request) {
 	db.UpdateOperationsTime(timePlus, timeMinus, timeMultiply, timeDivide)
 }
 
+func ComputingResourcesHandler(w http.ResponseWriter, r *http.Request) {
+	computingResourcesTemplate.Execute(w, threadInfos)
+}
+
 func RecoverMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
@@ -96,19 +115,40 @@ func ParsingMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	})
 }
 
-func init() {
-	for range 10 {
-		go func() {
-			for exp := range expressionsChan {
-				exp.Status = "calculating"
-				db.UpdateStatus(exp)
-
-				exp.Calculate()
-
-				db.UpdateStatus(exp)
-				db.UpdateResult(exp)
+func threadFunc(threadInfo *ThreadInfo) {
+	ticker := time.NewTicker(5 * time.Second)
+	defer ticker.Stop()
+	for {
+		select {
+		case <-ticker.C:
+			threadInfo.UpdateStatus("Waiting for expression")
+		case exp, ok := <-expressionsChan:
+			if !ok {
+				threadInfo.UpdateStatus("Closed")
+				return
 			}
-		}()
+
+			threadInfo.UpdateStatus(fmt.Sprintf("Calculation expression #%v", exp.Id))
+			exp.Status = "calculating"
+			db.UpdateStatus(exp)
+
+			exp.Calculate()
+
+			db.UpdateStatus(exp)
+			db.UpdateResult(exp)
+			threadInfo.UpdateStatus("Waiting for expression")
+		}
+	}
+}
+
+func init() {
+	for i := range numberOfThreads {
+		threadInfos[i] = &ThreadInfo{
+			LastPing: time.Now(),
+			Status:   "Waiting for expression",
+			Id:       i + 1,
+		}
+		go threadFunc(threadInfos[i])
 	}
 
 	exps := db.GetUncalculatingExpressions()
@@ -120,6 +160,7 @@ func init() {
 
 func main() {
 	defer db.Close()
+	defer close(expressionsChan)
 
 	r := mux.NewRouter()
 	r.Use(RecoverMiddleware)
@@ -128,6 +169,8 @@ func main() {
 	r.HandleFunc("/list-expressions", ListExpressionsHandler).Methods("GET")
 	r.HandleFunc("/configuration", ConfigurationHandler).Methods("GET")
 	r.HandleFunc("/configuration/change", ChangeConfigurationHandler).Methods("PUT")
+	r.HandleFunc("/computing-resources", ComputingResourcesHandler).Methods("GET")
+	// r.HandleFunc("/computing-resources/change", ComputingResourcesHandler).Methods("GET")
 
 	fs := http.FileServer(http.Dir("static"))
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static", fs))
